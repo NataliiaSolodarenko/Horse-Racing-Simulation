133364acf00e63d626ea24bd51dc3f1956fa6013
Nataliia Solodarenko <nataliiasolodarenko@gmail.com>
2025-07-10 19:56:35 +0300

    First Commit

---
+++ PracTask/PracTask/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PracTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.horse1 = new System.Windows.Forms.Label();
+            this.horse2 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.horse3 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.horse4 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.horse5 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.resultsDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.ForeColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(93, 49);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(685, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // horse1
+            // 
+            this.horse1.AutoSize = true;
+            this.horse1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.horse1.ForeColor = System.Drawing.Color.White;
+            this.horse1.Location = new System.Drawing.Point(12, 52);
+            this.horse1.Name = "horse1";
+            this.horse1.Size = new System.Drawing.Size(82, 20);
+            this.horse1.TabIndex = 1;
+            this.horse1.Text = "Horse #1";
+            // 
+            // horse2
+            // 
+            this.horse2.AutoSize = true;
+            this.horse2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.horse2.ForeColor = System.Drawing.Color.White;
+            this.horse2.Location = new System.Drawing.Point(12, 101);
+            this.horse2.Name = "horse2";
+            this.horse2.Size = new System.Drawing.Size(82, 20);
+            this.horse2.TabIndex = 3;
+            this.horse2.Text = "Horse #2";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.BackColor = System.Drawing.Color.White;
+            this.progressBar2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar2.Location = new System.Drawing.Point(93, 98);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(685, 23);
+            this.progressBar2.TabIndex = 2;
+            // 
+            // horse3
+            // 
+            this.horse3.AutoSize = true;
+            this.horse3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.horse3.ForeColor = System.Drawing.Color.White;
+            this.horse3.Location = new System.Drawing.Point(12, 150);
+            this.horse3.Name = "horse3";
+            this.horse3.Size = new System.Drawing.Size(82, 20);
+            this.horse3.TabIndex = 5;
+            this.horse3.Text = "Horse #3";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.BackColor = System.Drawing.Color.White;
+            this.progressBar3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar3.Location = new System.Drawing.Point(93, 147);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(685, 23);
+            this.progressBar3.TabIndex = 4;
+            // 
+            // horse4
+            // 
+            this.horse4.AutoSize = true;
+            this.horse4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.horse4.ForeColor = System.Drawing.Color.White;
+            this.horse4.Location = new System.Drawing.Point(12, 198);
+            this.horse4.Name = "horse4";
+            this.horse4.Size = new System.Drawing.Size(82, 20);
+            this.horse4.TabIndex = 7;
+            this.horse4.Text = "Horse #4";
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.BackColor = System.Drawing.Color.White;
+            this.progressBar4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar4.Location = new System.Drawing.Point(93, 195);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(685, 23);
+            this.progressBar4.TabIndex = 6;
+            // 
+            // horse5
+            // 
+            this.horse5.AutoSize = true;
+            this.horse5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.horse5.ForeColor = System.Drawing.Color.White;
+            this.horse5.Location = new System.Drawing.Point(12, 245);
+            this.horse5.Name = "horse5";
+            this.horse5.Size = new System.Drawing.Size(82, 20);
+            this.horse5.TabIndex = 9;
+            this.horse5.Text = "Horse #5";
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.BackColor = System.Drawing.Color.White;
+            this.progressBar5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar5.Location = new System.Drawing.Point(93, 242);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(685, 23);
+            this.progressBar5.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(323, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Horse Racing";
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.White;
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(681, 9);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(97, 28);
+            this.start.TabIndex = 13;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // resultsDataGridView
+            // 
+            this.resultsDataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.resultsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultsDataGridView.Location = new System.Drawing.Point(16, 303);
+            this.resultsDataGridView.Name = "resultsDataGridView";
+            this.resultsDataGridView.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.resultsDataGridView.Size = new System.Drawing.Size(200, 135);
+            this.resultsDataGridView.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultsDataGridView);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.horse5);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.horse4);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.horse3);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.horse2);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.horse1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Horse Racing";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.resultsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label horse1;
+        private System.Windows.Forms.Label horse2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label horse3;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label horse4;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.Label horse5;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.DataGridView resultsDataGridView;
+    }
+}
+
---
+++ PracTask/PracTask/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracTask
+{
+    public partial class Form1 : Form
+    {
+        private ProgressBar[] progressBars;
+        private List<(int horseNumber, int time)> raceResults;
+        private readonly object lockObject = new object();
+        private bool isStarted = false;
+        private bool isRaceCancelled;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBars = new ProgressBar[] { progressBar1, progressBar2, progressBar3, progressBar4, progressBar5 };
+        }
+
+        private async void start_Click(object sender, EventArgs e)
+        {
+            isRaceCancelled = false;
+
+            if (!isStarted)
+            {
+                isStarted = true;
+                start.Text = "Stop";
+                raceResults = new List<(int horseNumber, int time)>();
+                resultsDataGridView.Rows.Clear();
+
+                var barrier = new Barrier(progressBars.Length);
+                var tasks = new List<Task>();
+
+                for (int i = 0; i < progressBars.Length; i++)
+                {
+                    int horseIndex = i;
+                    var progressBar = progressBars[i];
+
+                    tasks.Add(Task.Run(() => RunHorse(horseIndex, progressBar, barrier)));
+
+                    await Task.Delay(50);
+                }
+
+                try
+                {
+                    await Task.WhenAll(tasks);
+                    if (!isRaceCancelled)
+                        DisplayResults();
+                }
+                catch
+                {
+                    MessageBox.Show("The race has been stopped!", "Stop");
+                }
+
+                isStarted = false;
+                start.Text = "Start";
+            }
+            else 
+            {
+                isRaceCancelled = true;
+                start.Text = "Start";
+                isStarted = false;
+            }
+        }
+
+        private void RunHorse(int horseIndex, ProgressBar progressBar, Barrier barrier)
+        {
+            var random = new Random();
+
+            barrier.SignalAndWait();
+
+            int progress = 0;
+            int elapsedTime = 0;
+
+            while (progress < 100)
+            {
+                if (isRaceCancelled)
+                    return;
+
+                int speed = random.Next(1, 5);
+                progress += speed;
+
+                if (progress > 100) progress = 100;
+                Invoke(new Action(() =>
+                {
+                    progressBar.Value = progress;
+                }));
+
+                Thread.Sleep(500);
+                elapsedTime += 500;
+            }
+
+            lock (lockObject)
+            {
+                raceResults.Add((horseIndex + 1, elapsedTime));
+            }
+        }
+
+        private void DisplayResults()
+        {
+            resultsDataGridView.ColumnCount = 1;
+            resultsDataGridView.Columns[0].Width = 100;
+            resultsDataGridView.RowHeadersWidth = 100;
+            resultsDataGridView.Columns[0].HeaderText = "Horse";
+
+            raceResults = raceResults.OrderBy(r => r.time).ToList();
+
+            foreach (var result in raceResults)
+            {
+                resultsDataGridView.Rows.Add($"Horse #{result.horseNumber}");
+            }
+
+            for(int i = 0; i < progressBars.Length; i++)
+                resultsDataGridView.Rows[i].HeaderCell.Value = (1+i) + " Place";
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isStarted)
+            {
+                e.Cancel = true;
+                MessageBox.Show("The race is still in progress. Please stop it before closing the window.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
